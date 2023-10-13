38d56a51f357856d4e2e26c059181d9d506247a1
Kamil Grzaba <kamil.grzaba@gmail.com>
2023-10-13 09:11:48 +0200

    [PPPSYS-43233] removed old examples (v12)

--- example/next.config.js
+++ example/next.config.js
@@ -1,10 +1,4 @@
 /** @type {import('next').NextConfig} */
+const nextConfig = {}
 
-const withTM = require('next-transpile-modules')(['@piwikpro/next-piwik-pro'])
-
-const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true
-}
-
-module.exports = withTM(nextConfig)
+module.exports = nextConfig

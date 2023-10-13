4e1cc6f4603fbb581f94a385b76acf414ce0819d
Kamil Grzaba <kamil.grzaba@gmail.com>
2023-10-13 09:12:27 +0200

    [PPPSYS-43233] removed unused import in layout

---
+++ example/app/layout.tsx
@@ -0,0 +1,26 @@
+'use client'
+
+import './globals.css'
+import {Inter} from 'next/font/google'
+import PiwikProProvider from '@piwikpro/next-piwik-pro'
+
+const inter = Inter({subsets: ['latin']})
+
+export default function RootLayout({
+                                     children
+                                   }: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en">
+    <body className={inter.className}>
+    <PiwikProProvider
+      containerUrl={process.env.NEXT_PUBLIC_CONTAINER_URL}
+      containerId={process.env.NEXT_PUBLIC_CONTAINER_ID}
+    >
+      {children}
+    </PiwikProProvider>
+    </body>
+    </html>
+  )
+}

22287e3a4b9bc02f5eebeb5e37d9df68287911bc
auto200 <michal.warac@gmail.com>
2023-11-24 22:50:27 +0100

    docs: annotate deprecated 'accountName' prop

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,6 +1,7 @@
 import { ReactNode } from 'react'
 
 export interface PiwikProProps {
+  /** @deprecated This parameter has now been replaced by 'containerId'. The 'accountName' parameter is deprecated and will be removed in the future. */
   accountName?: string
   containerId?: string
   containerUrl?: string

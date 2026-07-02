a8174a0c1ec2e03cac53fdaf76b58e9c56fe4185
Pawel Fajfer <p.fajfer@piwik.pro>
2026-07-02 13:43:02 +0200

    Added next compatibility tests in CI (#59)

--- example/__tests__/index-page.js
+++
@@ -1,7 +0,0 @@
-import {render} from '@testing-library/react'
-import IndexPage from '@/app/page.tsx'
-
-it('renders homepage unchanged', () => {
-  const {container} = render(<IndexPage/>)
-  expect(container).toMatchSnapshot()
-})
